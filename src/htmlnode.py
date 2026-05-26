2af3a6d7e920f1b3c1fff76f4e3b435807f110a8
Ron Ih <ronih@pacbell.net>
2026-05-25 22:00:33 -0700

    Added ParentNode and test solution files but still getting errors

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -37,17 +37,17 @@ class LeafNode(HTMLNode):
     
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag, children, props=None)
-        
-    def __to_html__(self):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
         if self.tag is None:
-            raise ValueError("Parent nodes must have a tag")
-        elif self.children is None:
-            raise ValueError("Parent nodes must have children")
-        else:
-            props_html = self.__props_to_html__(self.props) if self.props else ""
-            children_html = "".join([child.__to_html__() for child in self.children])
-            return f"<{self.tag}{props_html}>{children_html}</{self.tag}>"
-            
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
     def __repr__(self):
-        return f"ParentNode({self.tag}, {self.children}, {self.props})"
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
