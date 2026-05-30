5905556db560cd8e05d40ed4b587779d6f3dd5ab
Ron Ih <ronih@pacbell.net>
2026-05-30 16:32:53 -0700

    split_nodes_delimiter function code added

--- src/main.py
+++ src/main.py
@@ -17,6 +17,21 @@ def text_node_to_html_node(text_node):
     else:
         raise Exception(f"invalid text type: {text_node.text_type}")
 
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        else:
+            split_text = node.text.split(delimiter)
+            for i, text in enumerate(split_text):
+                if i > 0:
+                    new_nodes.append(TextNode(delimiter, text_type))
+                new_nodes.append(TextNode(text, text_type))
+        
+    return new_nodes
+
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
